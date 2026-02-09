854dc78fe44408e3a6acccf6c2773cb1e1a4d346
kmjak <tymkoron1221@icloud.com>
2026-02-09 18:11:05 +0900

    feat: ナビ案内画面の実装

---
+++ src/app/(main)/room/[room_id]/meet/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+import RouteGuidanceContainer from "@/features/map/components/RouteGuidanceContainer";
+
+export const metadata: Metadata = {
+  title: "ナビ案内",
+};
+
+export default function Meet() {
+  return <RouteGuidanceContainer />;
+}
