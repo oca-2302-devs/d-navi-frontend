183165119546ed4e76307bcad68b1782bb8a4aed
tkt12 <kinotaka1227@gmail.com>
2026-02-09 12:36:22 +0900

    ヘッダーの作成

---
+++ src/shared/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="flex h-16 w-full items-center bg-[#ff4064] px-4">
+      <h1 className="font-bold text-white">Dナビ</h1>
+    </header>
+  );
+}
