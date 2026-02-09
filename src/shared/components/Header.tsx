af1dc1541bf2f5cf0be6ed873fc31a23e18a59b4
tkt12 <kinotaka1227@gmail.com>
2026-02-09 15:36:17 +0900

    背景色の記述変更

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="flex h-16 w-full items-center bg-[#ff4064] px-4">
+    <header className="flex h-16 w-full items-center bg-rose-500 px-4">
       <h1 className="font-bold text-white">Dナビ</h1>
     </header>
   );
